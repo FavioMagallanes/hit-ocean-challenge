cdff05a82f1a644dd3869c2f55253a73b64beb3c
favioMagallanes <magallanes.f@outlook.com>
2023-07-12 16:31:48 -0300

    Add cart context

---
+++ src/context/CartProvider.tsx
@@ -0,0 +1,87 @@
+import { FC, useState, useEffect } from "react";
+import { CartContext, CartItem } from "./cartContext";
+import {
+  saveCartItemsToLocalStorage,
+  saveGemsToLocalStorage,
+} from "@/utils/localStorageUtils";
+import { CartContextType } from "@/types/interfaces";
+
+interface CartProviderProps {
+  children: React.ReactNode;
+}
+export const CartProvider: FC<CartProviderProps> = ({ children }) => {
+  const storedCartItems = localStorage.getItem("cartItems");
+  const initialCartItems: CartItem[] = storedCartItems
+    ? JSON.parse(storedCartItems)
+    : [];
+
+  const [cartItems, setCartItems] = useState<CartItem[]>(initialCartItems);
+
+  const storedGems = localStorage.getItem("gems");
+  const initialGems: number = storedGems ? parseInt(storedGems) : 3;
+
+  const [gems, setGems] = useState(initialGems);
+
+  useEffect(() => {
+    saveCartItemsToLocalStorage(cartItems);
+    saveGemsToLocalStorage(gems);
+  }, [cartItems, gems]);
+
+  const addItemToCart = (item: CartItem) => {
+    setCartItems(prevItems => {
+      const existingItemIndex = prevItems.findIndex(
+        cartItem => cartItem.id === item.id,
+      );
+
+      if (existingItemIndex !== -1) {
+        const updatedItems = [...prevItems];
+        updatedItems[existingItemIndex].cantidad += item.cantidad;
+        return updatedItems;
+      } else {
+        return [...prevItems, item];
+      }
+    });
+
+    setGems(prevGems => prevGems - item.precio * item.cantidad);
+  };
+
+  const removeItemFromCart = (itemId: number) => {
+    setCartItems(prevItems => prevItems.filter(item => item.id !== itemId));
+    const removedItem = cartItems.find(item => item.id === itemId);
+
+    if (removedItem)
+      setGems(prevGems => prevGems + removedItem.precio * removedItem.cantidad);
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+    setGems(3);
+  };
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      saveCartItemsToLocalStorage(cartItems);
+      saveGemsToLocalStorage(gems);
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [cartItems, gems]);
+
+  const cartContextValue: CartContextType = {
+    cartItems,
+    gems,
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+  };
+
+  return (
+    <CartContext.Provider value={cartContextValue}>
+      {children}
+    </CartContext.Provider>
+  );
+};
---
+++ src/context/cartContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+import { Product } from "@/types/interfaces";
+
+export interface CartItem extends Product {
+  cantidad: number;
+}
+
+interface CartContextType {
+  cartItems: CartItem[];
+  gems: number;
+  addItemToCart: (item: CartItem) => void;
+  removeItemFromCart: (itemId: number) => void;
+  clearCart: () => void;
+}
+
+export const CartContext = createContext<CartContextType>({
+  cartItems: [],
+  gems: 3,
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearCart: () => {},
+});
+
+export const useCartContext = () => useContext(CartContext);
